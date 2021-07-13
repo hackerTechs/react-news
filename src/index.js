a4453e5826581141baeaef96d074a73e48ebbb63
hackerTechs <54631799+hackerTechs@users.noreply.github.com>
2021-07-13 23:42:09 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import './css/style.css';
-import './css/owl.carousel.css';
-import './js/owl.carousel.js';
 
 import App from './App';
 
